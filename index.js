d07bfa8b1ea9685f8077b3c90c5b687eeae2d844
Joyce Chun <wooyangee@gmail.com>
2018-04-05 06:15:59 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
-var recipes = { typesOf: "korean" }
+var recipes = { eggs: "3" }
 
 function updateObjectWithKeyAndValue(object, key, value){
 return Object.assign({}, obj, { [key]: value })
 }
+updateObjectWithKeyAndValue(recipe, 'chocolate', '1 cup')
