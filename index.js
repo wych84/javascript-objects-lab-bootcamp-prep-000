254dc4ba560fe24b2164177b725e9728713beea9
Joyce Chun <wooyangee@gmail.com>
2018-04-05 06:18:18 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,7 +3,5 @@ var recipes = { eggs: "3" }
 function updateObjectWithKeyAndValue(object, key, value) {
   return object.assign({}, obj, { [key]: value});
 }
-function updateObjectWithKeyAndValue(object, key, value){
-return Object.assign({}, obj, { [key]: value })
-}
+
 updateObjectWithKeyAndValue(recipe, 'chocolate', '1 cup')
